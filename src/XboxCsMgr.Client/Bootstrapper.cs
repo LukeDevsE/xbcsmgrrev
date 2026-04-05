babfc5bc304439c7fe04e37863904d498eb28214
LukeDevsE <108841495+LukeDevsE@users.noreply.github.com>
2026-04-04 23:16:33 -0600

    persistent login
    you can stay logged in, as it saves your token. and also removed a little bit of useless dependencies

--- src/XboxCsMgr.Client/Bootstrapper.cs
+++ src/XboxCsMgr.Client/Bootstrapper.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Stylet;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -34,6 +36,31 @@ namespace XboxCsMgr.Client
         protected override async void OnStart()
         {
             Debug.WriteLine("Start program");
+            var RefToken = await GetRefreshToken();
+            authenticateService = new AuthenticateService(XblConfig);
+            if (RefToken != "")
+            {
+                HttpRequestMessage req3 = new HttpRequestMessage(HttpMethod.Post, $"https://login.microsoftonline.com/consumers/oauth2/v2.0/token");
+                req3.Method = HttpMethod.Post;
+                req3.Content = new StringContent($"client_id={CLIENT_ID}&scope=Xboxlive.signin%20Xboxlive.offline_access&grant_type=refresh_token&refresh_token={RefToken}", System.Text.Encoding.UTF8, "application/x-www-form-urlencoded"); // have fun anyone looking at this crappy code
+                HttpResponseMessage refreshresponse = await devicecode.SendAsync(req3);
+                string refreshedtoken = await refreshresponse.Content.ReadAsStringAsync();
+                var TableRefToken = JObject.Parse(refreshedtoken);
+                if (refreshresponse.IsSuccessStatusCode && TableRefToken != null && TableRefToken["access_token"] != null && TableRefToken["refresh_token"] != null)
+                {
+                    var finaltoken2 = await authenticateService.AuthenticateUser(TableRefToken["access_token"].ToString(), "d=");
+                    var result2 = await authenticateService.AuthorizeXsts(finaltoken2.Token);
+                    SaveRefreshToken(TableRefToken["refresh_token"].ToString());
+                    if (result2 != null)
+                    {
+                        //Debug.WriteLine("Authorized! Token: " + result.Token);
+                        XblConfig = new XboxLiveConfig(result2.Token, result2.DisplayClaims.XboxUserIdentity[0]);
+                        this.RootViewModel.OnAuthComplete();
+                    }
+                    base.OnStart();
+                    return;
+                }
+            }
             var dialog = new Dialogue();
             await Task.Yield();
             HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, $"https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode");
@@ -52,9 +79,9 @@ namespace XboxCsMgr.Client
             HttpResponseMessage tokenresponse = await devicecode.SendAsync(req2);
             string content2 = await tokenresponse.Content.ReadAsStringAsync();
             dynamic tokencode = JsonConvert.DeserializeObject(content2);
-            authenticateService = new AuthenticateService(XblConfig);
             var finaltoken = await authenticateService.AuthenticateUser(tokencode["access_token"].ToString(), "d=");
             var result = await authenticateService.AuthorizeXsts(finaltoken.Token);
+            SaveRefreshToken(tokencode["refresh_token"].ToString());
             if (result != null)
             {
                 //Debug.WriteLine("Authorized! Token: " + result.Token);
@@ -63,9 +90,31 @@ namespace XboxCsMgr.Client
             }
             base.OnStart();
         }
-
-        // no longer needed
-
+        string AppDataxbcsDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "xbcsmgrrev");
+        private async Task<string> GetRefreshToken()
+        {
+            var filepath = Path.Combine(AppDataxbcsDir, "reftoken");
+            if (File.Exists(filepath) && Directory.Exists(AppDataxbcsDir))
+            {
+                var readtoken = await File.ReadAllTextAsync(filepath);
+                return readtoken;
+            } 
+            else
+            {
+                return "";
+            }
+            // you'll never know how ugly this code was before
+        }
+        private async void SaveRefreshToken(string token)
+        {
+            if (!Directory.Exists(AppDataxbcsDir))
+            {
+                Directory.CreateDirectory(AppDataxbcsDir);
+            }
+            await File.WriteAllTextAsync(Path.Combine(AppDataxbcsDir, "reftoken"), token); // maybe i could make some sort of cache system for the preview of the worlds in mc xb1.. hmmmm....
+        }
+        // nolonger needed
+        /*
         private void LoadXblTokenCredentials()
         {
             // Lookup current Xbox Live authentication data stored via wincred
@@ -97,5 +146,6 @@ namespace XboxCsMgr.Client
                 }
             }
         }
+        */
     }
 }
--- src/XboxCsMgr.XboxLive/XboxLiveService.cs
+++ src/XboxCsMgr.XboxLive/XboxLiveService.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Org.BouncyCastle.Ocsp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
